8fe49bfabe6b0d231e6762ff85245ac549bdf0a9
Zvampish <nornostvold02@gmail.com>
2020-03-11 11:08:36 +0100

    Lektion 2

--- Slutprojektet/Program.cs
+++ Slutprojektet/Program.cs
@@ -10,7 +10,33 @@ namespace Slutprojektet
     {
         static void Main()
         {
-            Console.WriteLine("Welcome to my house!");
+            string[] cellItems = {"File", "NrG Drink","Teleporter","Rocket Launcher","Cellphone","Drill"};
+
+            //string[];
+
+            string location = "";
+
+            while (location != "outside")
+            {
+                Console.WriteLine("You open your eyes and realize you are in a cell");
+                Console.WriteLine("");
+                Console.WriteLine("You see six different items spread out on the floor:");
+                Console.WriteLine("");
+
+                for (int i = 0; i < cellItems.Length; i++)
+                {
+                    Console.WriteLine(i + 1 + " " + cellItems[i]);
+                }
+
+                Console.WriteLine("Choose wich item you want to use by pressing the corresponding number. (Don't use your numpad, it won't work)");
+
+                if (Console.ReadKey().Key == ConsoleKey.D1)
+                {
+                    Console.WriteLine("\nbruh");
+                }
+
+                Console.ReadLine();
+            }
         }
     }
 }
