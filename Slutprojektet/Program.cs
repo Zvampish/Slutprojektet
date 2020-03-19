e036dc9ec4aa605b6397487ddf9f84d167c49482
Zvampish <nornostvold02@gmail.com>
2020-03-19 13:36:20 +0100

    Metod som omvandlar string till int

--- Slutprojektet/Program.cs
+++ Slutprojektet/Program.cs
@@ -10,7 +10,11 @@ namespace Slutprojektet
     {
         static void Main()
         {
-            string[] cellItems = {"File", "NrG Drink","Teleporter","Rocket Launcher","Cellphone","Drill"};
+            string name = "dummy";
+
+            string[] names = { "Bob", "Tom", "Man" };
+
+            string[] cellItems = { "File", "NrG Drink", "Teleporter", "Rocket Launcher", "Cellphone", "Drill" };
 
             //string[];
 
@@ -19,24 +23,58 @@ namespace Slutprojektet
             while (location != "outside")
             {
                 Console.WriteLine("You open your eyes and realize you are in a cell");
-                Console.WriteLine("");
-                Console.WriteLine("You see six different items spread out on the floor:");
-                Console.WriteLine("");
+
+                Console.WriteLine("\nA prison officer comes and visits your cell, what's your name he asks.");
+
+                for (int i = 0; i < names.Length; i++)
+                {
+                    Console.WriteLine(i + 1 + " " + names[i]);
+                }
+
+                Console.WriteLine("\nChoose wich name you want by typing the corresponding number.");
+                
+                string userInpt = Console.ReadLine();
+
+                Console.WriteLine();
+                
+                if (MakeStringToInt(userInpt) == 1)
+                {
+                    name = names[0];
+                    Console.Clear();
+                }
+                else if (MakeStringToInt(userInpt) == 2)
+                {
+                    name = names[1];
+                    Console.Clear();
+                }
+                else if (MakeStringToInt(userInpt) == 3)
+                {
+                    name = names[2];
+                    Console.Clear();
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.Write("Since you didn't type a whole number between 1 and 3: ");
+                }
+
+                Console.WriteLine("Your name is now " + name + ".");
+
+                Console.WriteLine("\nYou see six different items spread out on the floor:");
 
                 for (int i = 0; i < cellItems.Length; i++)
                 {
                     Console.WriteLine(i + 1 + " " + cellItems[i]);
                 }
-
-                Console.WriteLine("Choose wich item you want to use by pressing the corresponding number. (Don't use your numpad, it won't work)");
-
-                if (Console.ReadKey().Key == ConsoleKey.D1)
-                {
-                    Console.WriteLine("\nbruh");
-                }
-
-                Console.ReadLine();
+                    
+                Console.WriteLine("Choose wich item you want to pick up and use by typing the corresponding number.");
             }
+        }
+        static int MakeStringToInt(string userInpt)
+        {
+            int.TryParse(userInpt, out int userInptInt);
+
+            return userInptInt;
         }
     }
 }
