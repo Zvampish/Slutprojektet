83821387dcf3f2018e6e75c06819d193b20fdffc
Zvampish <nornostvold02@gmail.com>
2020-03-25 11:08:06 +0100

    Programmet anpassar sig efter fel input

--- Slutprojektet/Program.cs
+++ Slutprojektet/Program.cs
@@ -10,64 +10,105 @@ namespace Slutprojektet
     {
         static void Main()
         {
+
             string name = "dummy";
+
+            string item;
 
             string[] names = { "Bob", "Tom", "Man" };
 
             string[] cellItems = { "File", "NrG Drink", "Teleporter", "Rocket Launcher", "Cellphone", "Drill" };
 
-            //string[];
+            //I slutet ska man skriva in en kod som baseras på de valen man väljer. Om man valde det tredje alternativet i den första så ska man 
 
             string location = "";
 
-            while (location != "outside")
-            {
+            while (location != "outside"){
                 Console.WriteLine("You open your eyes and realize you are in a cell");
 
                 Console.WriteLine("\nA prison officer comes and visits your cell, what's your name he asks.");
 
-                for (int i = 0; i < names.Length; i++)
-                {
-                    Console.WriteLine(i + 1 + " " + names[i]);
-                }
+                bool validAnswer = false;
 
-                Console.WriteLine("\nChoose wich name you want by typing the corresponding number.");
-                
-                string userInpt = Console.ReadLine();
+                while (validAnswer == false){
+                    for (int i = 0; i < names.Length; i++){
+                        Console.WriteLine(i + 1 + " " + names[i]);
+                    }
+
+                    Console.WriteLine("\nChoose wich name you want by typing the corresponding number.");
+
+                    string userInpt = Console.ReadLine();
+
+                    if (MakeStringToInt(userInpt) == 1){
+                        name = names[0];
+                        validAnswer = true;
+                    }
+                    else if (MakeStringToInt(userInpt) == 2){
+                        name = names[1];
+                        validAnswer = true;
+                    }
+                    else if (MakeStringToInt(userInpt) == 3){
+                        name = names[2];
+                        validAnswer = true;
+                    }
+                    else{
+                        Console.Write("Type an integer between 1-3");
+                        Console.WriteLine("Press any key to try again");
+                        Console.ReadKey();
+                        Console.Clear();
+                    }
+                }
 
                 Console.WriteLine();
-                
-                if (MakeStringToInt(userInpt) == 1)
-                {
-                    name = names[0];
-                    Console.Clear();
-                }
-                else if (MakeStringToInt(userInpt) == 2)
-                {
-                    name = names[1];
-                    Console.Clear();
-                }
-                else if (MakeStringToInt(userInpt) == 3)
-                {
-                    name = names[2];
-                    Console.Clear();
-                }
-                else
-                {
-                    Console.Clear();
-                    Console.Write("Since you didn't type a whole number between 1 and 3: ");
-                }
 
                 Console.WriteLine("Your name is now " + name + ".");
 
-                Console.WriteLine("\nYou see six different items spread out on the floor:");
+                validAnswer = false;
 
-                for (int i = 0; i < cellItems.Length; i++)
-                {
-                    Console.WriteLine(i + 1 + " " + cellItems[i]);
+                while (validAnswer == false){
+
+                    Console.WriteLine("\nYou see six different items spread out on the floor:");
+
+                    for (int i = 0; i < cellItems.Length; i++)
+                    {
+                        Console.WriteLine(i + 1 + " " + cellItems[i]);
+                    }
+
+                    Console.WriteLine("\nChoose which item you want to pick up and use by typing the corresponding number.");
+
+                    string userInpt = Console.ReadLine();
+
+                    if (MakeStringToInt(userInpt) == 1){
+                        item = cellItems[0];
+                        validAnswer = true;
+                    }
+                    else if (MakeStringToInt(userInpt) == 2){
+                        item = cellItems[1];
+                        validAnswer = true;
+                    }
+                    else if (MakeStringToInt(userInpt) == 3){
+                        item = cellItems[2];
+                        validAnswer = true;
+                    }
+                    else if (MakeStringToInt(userInpt) == 4){
+                        item = cellItems[3];
+                        validAnswer = true;
+                    }
+                    else if (MakeStringToInt(userInpt) == 5){
+                        item = cellItems[4];
+                        validAnswer = true;
+                    }
+                    else if (MakeStringToInt(userInpt) == 6){
+                        item = cellItems[5];
+                        validAnswer = true;
+                    }
+                    else{
+                        Console.WriteLine("Type an integer between 1-6");
+                        Console.WriteLine("Press any key to try again");
+                        Console.ReadKey();
+                        Console.Clear();
+                    }
                 }
-                    
-                Console.WriteLine("Choose wich item you want to pick up and use by typing the corresponding number.");
             }
         }
         static int MakeStringToInt(string userInpt)
