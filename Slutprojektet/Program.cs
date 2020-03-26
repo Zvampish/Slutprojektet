2d21e02eddc0a02c05ca846cbecdb32d75d998b4
Zvampish <nornostvold02@gmail.com>
2020-03-26 12:15:40 +0100

    Mer kod (En kommentar)

--- Slutprojektet/Program.cs
+++ Slutprojektet/Program.cs
@@ -10,14 +10,15 @@ namespace Slutprojektet
     {
         static void Main()
         {
+            string name = "";
 
-            string name = "dummy";
+            string item = "";
 
-            string item;
+            //Använder två arrayer nedan eftersom det endast ska finnas dessa bestämda items och names.
 
             string[] names = { "Bob", "Tom", "Man" };
 
-            string[] cellItems = { "File", "NrG Drink", "Teleporter", "Rocket Launcher", "Cellphone", "Drill" };
+            string[] cellItems = { "The File", "The NrG Drink", "The Teleporter", "The Rocket Launcher", "The Cellphone", "The Drill" };
 
             //I slutet ska man skriva in en kod som baseras på de valen man väljer. Om man valde det tredje alternativet i den första så ska man 
 
@@ -37,23 +38,24 @@ namespace Slutprojektet
 
                     Console.WriteLine("\nChoose wich name you want by typing the corresponding number.");
 
-                    string userInpt = Console.ReadLine();
+                    string userNameInpt = Console.ReadLine();
 
-                    if (MakeStringToInt(userInpt) == 1){
+                    if (MakeStringToInt(userNameInpt) == 1){
                         name = names[0];
                         validAnswer = true;
                     }
-                    else if (MakeStringToInt(userInpt) == 2){
+                    else if (MakeStringToInt(userNameInpt) == 2){
                         name = names[1];
                         validAnswer = true;
                     }
-                    else if (MakeStringToInt(userInpt) == 3){
+                    else if (MakeStringToInt(userNameInpt) == 3){
                         name = names[2];
                         validAnswer = true;
                     }
                     else{
-                        Console.Write("Type an integer between 1-3");
-                        Console.WriteLine("Press any key to try again");
+                        Console.Clear();
+                        Console.WriteLine("Type an integer between 1-3");
+                        Console.WriteLine("\nPress any key to try again");
                         Console.ReadKey();
                         Console.Clear();
                     }
@@ -76,29 +78,29 @@ namespace Slutprojektet
 
                     Console.WriteLine("\nChoose which item you want to pick up and use by typing the corresponding number.");
 
-                    string userInpt = Console.ReadLine();
+                    string userTtemInpt = Console.ReadLine().Trim();
 
-                    if (MakeStringToInt(userInpt) == 1){
+                    if (MakeStringToInt(userTtemInpt) == 1){
                         item = cellItems[0];
                         validAnswer = true;
                     }
-                    else if (MakeStringToInt(userInpt) == 2){
+                    else if (MakeStringToInt(userTtemInpt) == 2){
                         item = cellItems[1];
                         validAnswer = true;
                     }
-                    else if (MakeStringToInt(userInpt) == 3){
+                    else if (MakeStringToInt(userTtemInpt) == 3){
                         item = cellItems[2];
                         validAnswer = true;
                     }
-                    else if (MakeStringToInt(userInpt) == 4){
+                    else if (MakeStringToInt(userTtemInpt) == 4){
                         item = cellItems[3];
                         validAnswer = true;
                     }
-                    else if (MakeStringToInt(userInpt) == 5){
+                    else if (MakeStringToInt(userTtemInpt) == 5){
                         item = cellItems[4];
                         validAnswer = true;
                     }
-                    else if (MakeStringToInt(userInpt) == 6){
+                    else if (MakeStringToInt(userTtemInpt) == 6){
                         item = cellItems[5];
                         validAnswer = true;
                     }
@@ -108,6 +110,14 @@ namespace Slutprojektet
                         Console.ReadKey();
                         Console.Clear();
                     }
+
+                    Console.WriteLine();
+
+                    Console.WriteLine("Your picked up " + item + ".");
+
+                    Console.ReadLine();
+
+                    location = "outside";
                 }
             }
         }
